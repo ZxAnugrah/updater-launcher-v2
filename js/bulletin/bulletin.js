00189decc518419a8fa33bd7c04f701ccb74978e
ZxAnugrah <anugrahsukmono@gmail.com>
2026-04-14 13:34:51 +0700

    Update for 15.04.26

--- js/bulletin/bulletin.js
+++ js/bulletin/bulletin.js
@@ -1,51 +1,56 @@
-function etc_011023() {
-  alert("Lorem ipsum dolor sit amet\ndsajdwd");
+function sys_150426() {
+  showCustomAlert(
+    "Update 15/04/2026\n\nTelah rilis 'Updater Launcher Bahasa Indonesia Fix V2.0'.\nFitur baru yang ditambahkan:\n - Kustom direktori instalasi Counter-Strike Nexon.\n - Tombol 'Game Start' (Steam wajib dijalankan dan telah login pada akun yang akan dimainkan).\n - Upgrade SDK ke .NET 8.0.\n\nTerima kasih untuk tester Updater Launcher terbaru, Oscar Imannuel (GeludSkuy).",
+  );
 }
 
 function sys_041023() {
-  alert("Update 04/10/2023\n\nTelah rilis 'Updater Application Bahasa Indonesia Fix (Beta)'.\n\nUntuk versi kali ini masih dalam tahap pengembangan dan akan diperbarui kedepannya!");
+  showCustomAlert("Update 04/10/2023\n\nTelah rilis 'Updater Application Bahasa Indonesia Fix (Beta)'.\n\nUntuk versi kali ini masih dalam tahap pengembangan dan akan diperbarui kedepannya!");
 }
 
 // ============================================================================================================================
 
+function etc_150426() {
+  showCustomAlert("Update 15/04/2026\n\nMemperbarui tampilan Change Logs agar menyesuaikan dengan launcher terbaru.");
+}
+
 function etc_140126() {
-  alert("Update 14/01/2026\n\nMenghapus beberapa bulletin lama.\nSekarang bulletin akan direset setiap setahun sekali.");
+  showCustomAlert("Update 14/01/2026\n\nMenghapus beberapa bulletin lama.\nSekarang bulletin akan direset setiap setahun sekali.");
 }
 
 // Update Client Patch CSNS 14.01.26
 function upd_140126() {
-  alert("Update 14/01/2026:\n======================================\n* V1.1.40.1\n- New weapons\n- New medals\n- New items\n- New map\n- New mode\n- New events\n- New systems\n\n(Selengkapnya, check pada Change Logs)");
+  showCustomAlert("Update 14/01/2026:\n\n* V1.1.40.1\n- New weapons\n- New medals\n- New items\n- New map\n- New mode\n- New events\n- New systems\n\n(Selengkapnya, check pada Change Logs)");
 }
 
 // Update Client Patch CSN 11.02.26
 function upd_110226() {
-  alert("Update 11/02/2026:\n======================================\n* V1.1.41.1\n- New weapons\n- New medals\n- New items\n- New map\n- New mode\n- New events\n- New systems\n\n(Selengkapnya, check pada Change Logs)");
+  showCustomAlert("Update 11/02/2026:\n\n* V1.1.41.1\n- New weapons\n- New medals\n- New items\n- New map\n- New mode\n- New events\n- New systems\n\n(Selengkapnya, check pada Change Logs)");
 }
 
 // Update Client Patch CSN 18.03.26
 function upd_180326() {
-  alert("Update 18/03/2026:\n======================================\n* V1.1.42.1\n- New weapons\n- New medals\n- New items\n- New map\n- New season\n- New events\n- New systems\n\n(Selengkapnya, check pada Change Logs)");
+  showCustomAlert("Update 18/03/2026:\n\n* V1.1.42.1\n- New weapons\n- New medals\n- New items\n- New map\n- New season\n- New events\n- New systems\n\n(Selengkapnya, check pada Change Logs)");
 }
 
 // Update Client Patch CSN 02.04.26
 function upd_020426() {
-  alert("Update 02/04/2026:\n======================================\n* V1.1.42.2\n- New weapons\n- Fix strings\n- New events\n- New systems");
+  showCustomAlert("Update 02/04/2026:\n\n* V1.1.42.2\n- New weapons\n- Fix strings\n- New events\n- New systems");
+}
+
+// Update Client Patch CSN 15.04.26
+function upd_150426() {
+  showCustomAlert("Update 15/04/2026:\n\n* V2.1.43.1\n- New weapons\n- New medals\n- New items\n- New map\n- New season\n- New events\n- New systems\n\n(Selengkapnya, check pada Change Logs)");
 }
 
 /* Bug Logs */
 
 // Fix Desc Client Patch CSN 07.11.25
 function bug_logs_071125() {
-  alert("Update 07/11/2025:\n======================================\n* V1.1.37.2\n- Fix weapon paint strings.\n- Fix medal string.");
+  showCustomAlert("Update 07/11/2025:\n\n* V1.1.37.2\n- Fix weapon paint strings.\n- Fix medal string.");
 }
 
 // Fix String Client Patch CSN 12.02.26
 function bug_logs_120226() {
-  alert("Update 07/11/2025:\n======================================\n* V1.1.41.2\n- Fix deskripsi item pada Epic Shop.");
-}
-
-/* Divider */
-
-function img2_desc() {
-  alert("Version: 1.1.42.2\n\n- New weapons\n- Fix strings\n- New items\n- New events\n- New systems");
+  showCustomAlert("Update 12/02/2026:\n\n* V1.1.41.2\n- Fix deskripsi item pada Epic Shop.");
 }
