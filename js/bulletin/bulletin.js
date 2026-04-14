a9a214c15c82b71fd7dfceae698347ba79b7b306
ZxAnugrah <anugrahsukmono@gmail.com>
2026-04-14 12:06:53 +0700

    Upload sites to new sites

---
+++ js/bulletin/bulletin.js
@@ -0,0 +1,51 @@
+function etc_011023() {
+  alert("Lorem ipsum dolor sit amet\ndsajdwd");
+}
+
+function sys_041023() {
+  alert("Update 04/10/2023\n\nTelah rilis 'Updater Application Bahasa Indonesia Fix (Beta)'.\n\nUntuk versi kali ini masih dalam tahap pengembangan dan akan diperbarui kedepannya!");
+}
+
+// ============================================================================================================================
+
+function etc_140126() {
+  alert("Update 14/01/2026\n\nMenghapus beberapa bulletin lama.\nSekarang bulletin akan direset setiap setahun sekali.");
+}
+
+// Update Client Patch CSNS 14.01.26
+function upd_140126() {
+  alert("Update 14/01/2026:\n======================================\n* V1.1.40.1\n- New weapons\n- New medals\n- New items\n- New map\n- New mode\n- New events\n- New systems\n\n(Selengkapnya, check pada Change Logs)");
+}
+
+// Update Client Patch CSN 11.02.26
+function upd_110226() {
+  alert("Update 11/02/2026:\n======================================\n* V1.1.41.1\n- New weapons\n- New medals\n- New items\n- New map\n- New mode\n- New events\n- New systems\n\n(Selengkapnya, check pada Change Logs)");
+}
+
+// Update Client Patch CSN 18.03.26
+function upd_180326() {
+  alert("Update 18/03/2026:\n======================================\n* V1.1.42.1\n- New weapons\n- New medals\n- New items\n- New map\n- New season\n- New events\n- New systems\n\n(Selengkapnya, check pada Change Logs)");
+}
+
+// Update Client Patch CSN 02.04.26
+function upd_020426() {
+  alert("Update 02/04/2026:\n======================================\n* V1.1.42.2\n- New weapons\n- Fix strings\n- New events\n- New systems");
+}
+
+/* Bug Logs */
+
+// Fix Desc Client Patch CSN 07.11.25
+function bug_logs_071125() {
+  alert("Update 07/11/2025:\n======================================\n* V1.1.37.2\n- Fix weapon paint strings.\n- Fix medal string.");
+}
+
+// Fix String Client Patch CSN 12.02.26
+function bug_logs_120226() {
+  alert("Update 07/11/2025:\n======================================\n* V1.1.41.2\n- Fix deskripsi item pada Epic Shop.");
+}
+
+/* Divider */
+
+function img2_desc() {
+  alert("Version: 1.1.42.2\n\n- New weapons\n- Fix strings\n- New items\n- New events\n- New systems");
+}
